67199aea32d77836b4838959f45dfa7c4fc1a97f
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2020-03-01 12:55:08 +0530

    New module telegraph.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -121,6 +121,9 @@ YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
 # Default .alive name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
+#telegraph
+TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "TG-UBotX")
+
 # Time & Date - Country and Time Zone
 COUNTRY = str(os.environ.get("COUNTRY", ""))
 TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
