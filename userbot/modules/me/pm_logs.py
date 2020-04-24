4516654998a484ac377d8331bbb01677911a0139
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2020-04-24 11:45:07 +0530

    try

--- userbot/modules/me/pm_logs.py
+++ userbot/modules/me/pm_logs.py
@@ -16,6 +16,7 @@ from userbot import BOTLOG, BOTLOG_CHATID, LOGS
 
 
 NO_PM_LOG_USERS = []
+GROUP = [-1001150051137]
 
 
 @register(incoming=True, disable_edited=True)
@@ -23,7 +24,7 @@ async def monito_p_m_s(event):
     sender = await event.get_sender()
     if event.is_private and not (await event.get_sender()).bot:
         chat = await event.get_chat()
-        if chat.id not in NO_PM_LOG_USERS and chat.id:
+        if chat.id not in (NO_PM_LOG_USERS, GROUP) and chat.id:
             try:
                 e = await event.client.get_entity(int(BOTLOG_CHATID))
                 fwd_message = await event.client.forward_messages(
