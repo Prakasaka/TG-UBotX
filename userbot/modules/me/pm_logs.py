8cb6b531db652ba8c13d0bb950b1ab2e0113321a
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2020-04-24 12:14:18 +0530

    Update pm_logs.py

--- userbot/modules/me/pm_logs.py
+++ userbot/modules/me/pm_logs.py
@@ -16,13 +16,12 @@ from userbot import BOTLOG, BOTLOG_CHATID, LOGS
 
 
 NO_PM_LOG_USERS = []
-GROUP = -1001150051137
 
 
 @register(incoming=True, disable_edited=True)
 async def monito_p_m_s(event):
     sender = await event.get_sender()
-    if GROUP and not (await event.get_sender()).bot:
+    if event.is_private and not (await event.get_sender()).bot:
         chat = await event.get_chat()
         if chat.id not in NO_PM_LOG_USERS and chat.id:
             try:
