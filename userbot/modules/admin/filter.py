31d4bedbb8c25ea655ebec68ecc8db1b70017983
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2020-04-25 19:56:11 +0530

    Update filter.py

--- userbot/modules/admin/filter.py
+++ userbot/modules/admin/filter.py
@@ -48,7 +48,7 @@ async def add_new_filter(new_handler):
         await new_handler.edit("`Running on Non-SQL mode!`")
         return
     keyword = new_handler.pattern_match.group(1)
-    string = new_handler.partition(keyword)[2]
+    string = new_handler.text.partition(keyword)[2]
     msg = await new_handler.get_reply_message()
     msg_id = None
     if msg and msg.media and not string:
